b882d642a281dc31989959cd45e360452b73b4e4
Multyjog <simon.kotov.web@gmail.com>
2024-04-03 19:55:42 +0700

    - Initial commit

---
+++ FundMe.sol
@@ -0,0 +1,15 @@
+//  Get funds
+//  Withdraw funds
+//  Set a minimum funding value
+
+// SPD-License-Identifier: MIT
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract FundMe {
+    function fund() public {
+        
+    }
+    // function withdraw() public {}
+}
